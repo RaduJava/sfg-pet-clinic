ef0dfc0368a57db7884d78c9c72ce86e7bab32d0
Your NameRadugit config --global user.name Your <radugr@gmail.com>
2021-11-04 13:49:58 +0200

    display all the owners. Closes #17

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,12 +1,25 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("/owners")
-    public String listOwners() {
+    public String listOwners(Model model) {
+        Set<Owner> ownerSet = ownerService.findAll();
+        model.addAttribute("owners", ownerSet);
         return "owners/owners-list";
     }
 }
