9c757e1115e38d51b389d15531f2e7dfb792137e
Your NameRadugit config --global user.name Your <radugr@gmail.com>
2021-11-23 15:53:26 +0200

    Display Owner record (HTML). Closes #57

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/PetController.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("owners/{petId}")
+public class PetController {
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute(name = "owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @ModelAttribute(name = "types")
+    public Collection<PetType> populatePetType() {
+        return petTypeService.findAll();
+    }
+
+    @InitBinder("owner")
+    public void initBinder(WebDataBinder binder) {
+        binder.setDisallowedFields("id");
+    }
+}
