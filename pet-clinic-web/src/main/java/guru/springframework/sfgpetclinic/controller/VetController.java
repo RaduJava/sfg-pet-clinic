0df0cfc6dec58355f4cbaa83e82b7704c88d3380
Your NameRadugit config --global user.name Your <radugr@gmail.com>
2021-11-29 11:04:57 +0200

    Implement Vets JSON Endpoint. Closes #63

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -4,7 +4,9 @@ import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
@@ -21,5 +23,11 @@ public class VetController {
         Set<Vet> vetSet = vetService.findAll();
         model.addAttribute("vets", vetSet);
         return "vets/vets-list";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getJason() {
+        return vetService.findAll();
     }
 }
